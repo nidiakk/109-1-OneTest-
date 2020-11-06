e7217ca7b5ef925413ba70a0788fef953c3650ac
nidiakk <73993844+nidiakk@users.noreply.github.com>
2020-11-06 14:26:05 +0800

    Delete 3.login.php

--- 3.login.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    if (($_GET[id] == "john") && ($_GET[pwd]=="john1234"))#如果id等於john，pwd等於john1234
-        echo "Welcome";  #正確就輸出字串Welcome
-    else
-        echo "fail login";#錯誤就輸出字串fail login
-?>
